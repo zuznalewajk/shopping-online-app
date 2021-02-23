1ffcb83af86f7c2aaca362cdfc2f6944f1eaaef7
zuznalewajk <nalewajkzuzanna@gmail.com>
2021-02-23 17:46:55 +0100

    Initial commit

---
+++ src/test/java/com/github/shoppingonline/ShoppingOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.shoppingonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
